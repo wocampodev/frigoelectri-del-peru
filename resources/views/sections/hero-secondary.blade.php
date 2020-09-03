2e3018c8ae5c6ac22ab3ef352305e8dddfe1af01
Walter Ocampo <fearless347@gmail.com>
2020-09-02 19:33:36 -0500

    Contacto Plantilla

---
+++ resources/views/sections/hero-secondary.blade.php
@@ -0,0 +1,11 @@
+<section class="hero-wrap hero-wrap-2" style="background-image: url('{{ asset('assets/images/bg_2.jpg') }}');" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-end">
+            <div class="col-md-9 ftco-animate pb-5">
+                <p class="breadcrumbs mb-2"><span class="mr-2"><a href="{{ url('/') }}">Inicio <i class="ion-ios-arrow-forward"></i></a></span> <span>Contacto <i class="ion-ios-arrow-forward"></i></span></p>
+                <h1 class="mb-0 bread">Te asesoramos gratis!</h1>
+            </div>
+        </div>
+    </div>
+</section>
