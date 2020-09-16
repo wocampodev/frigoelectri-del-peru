c6885fb021d2c4b88a127f20af6c6d30718e2f1e
Walter Ocampo <fearless347@gmail.com>
2020-09-16 13:55:10 -0500

    Correcion imagen orden

--- resources/views/sections/detail.blade.php
+++ resources/views/sections/detail.blade.php
@@ -2,7 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 ftco-animate">
-              <p><img src="../assets/images/services/{{ $service->image }}" alt="" class="img-fluid"></p>
+              <p><img src="../assets/images/services/{{ $service->image }}" alt="" class="img-fluid" 
+                style="background-size: 33% 33%;
+                background-repeat: no-repeat;
+                background-position: center center;"></p>
               <h2 class="mb-3">{{ $service->name }}</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia suscipit, quisquam doloremque distinctio perferendis et doloribus unde architecto optio laboriosam porro adipisci sapiente officiis nemo accusamus ad praesentium? Esse minima nisi et. Dolore perferendis, enim praesentium omnis, iste doloremque quia officia optio deserunt molestiae voluptates soluta architecto tempora.</p>
             </div>
@@ -19,7 +22,7 @@
         </div>
     </div>
 </section>
-<section class="ftco-appointment ftco-section ftco-no-pt ftco-no-pb img" style="background-image: url('../assets/images/bg_3.jpg');">
+<section class="ftco-appointment ftco-section ftco-no-pt ftco-no-pb img" style="background-image: url('../assets/images/bg-inicioservicios.jpg');">
   <div class="overlay"></div>
   <div class="container">
     <div class="row d-md-flex justify-content-center">
