c33b7c4077f479e6568813ab0ca06f9c697d3335
Walter Ocampo <fearless347@gmail.com>
2020-09-16 16:44:00 -0500

    Correción notificación

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="{{ asset('vendor/css/jquery-ui.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/css/sweetalert2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/css/toastr.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('vendor/css/toastr.min.css') }}"> --}}
     {{-- <link rel="stylesheet" href="{{ asset('vendor/css/glider.min.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('css/web/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/web/codecix.css') }}">
@@ -59,7 +59,7 @@
         <script src="{{ asset('vendor/js/jquery-ui.min.js') }}"></script>
         <script src="{{ asset('vendor/js/scrollax.min.js') }}"></script>
         <script src="{{ asset('vendor/js/sweetalert2.min.js') }}"></script>
-        <script src="{{ asset('vendor/js/toastr.min.js') }}"></script>
+        {{-- <script src="{{ asset('vendor/js/toastr.min.js') }}"></script> --}}
         {{-- <script src="{{ asset('vendor/js/glider.min.js') }}"></script> --}}
         <script src="{{ asset('js/web/main.js') }}"></script>
         <script src="{{ asset('js/web/search.js') }}"></script>
